69d91076e45508be72f03c277a94366410a4b998
Professeur59 <65083916+Professeur59@users.noreply.github.com>
2023-05-07 01:42:00 +0200

    Bases Player
    Ajout du Idle du player, des déplacements de base

--- ZeldarkSouls/Assets/Script/Player.cs
+++ ZeldarkSouls/Assets/Script/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public GameObject GOPlayer;
+    public float speed = 100;
+    private Vector2 directionUp = new Vector2(0, 1);
+    private Vector2 directionLeft = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,27 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+        //Base déplacement du joueur
+        if(Input.GetKey("up"))
+        {
+            GOPlayer.transform.Translate(directionUp * speed * Time.deltaTime);                      
+        }
+
+        if(Input.GetKey("down"))
+        {
+            GOPlayer.transform.Translate(-directionUp * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            GOPlayer.transform.Translate(-directionLeft * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            GOPlayer.transform.Translate(directionLeft * speed * Time.deltaTime);
+        }
+
+       
     }
 }
